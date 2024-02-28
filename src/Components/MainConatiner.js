95e4b4fe2039ba5a524193b16df631b64ff8a588
adityaj358 <adityajiwane182000@gmail.com>
2024-02-28 18:31:18 +0530

    class 1

---
+++ src/Components/MainConatiner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ButtonsList from './ButtonsList'
+import VideoConatiner from './VideoConatiner'
+
+const MainConatiner = () => {
+  return (
+    <div>
+      <ButtonsList/>
+      <VideoConatiner/>
+    </div>
+  )
+}
+
+export default MainConatiner
